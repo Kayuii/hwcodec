1814b814257f787c77282d55b960c5bcca173b52
gecko <577738@qq.com>
2024-08-13 11:47:17 +0800

    fix unresolved external symbol

--- examples/available1.rs
+++ examples/available1.rs
@@ -1,16 +1,24 @@
 use env_logger::{init_from_env, Env, DEFAULT_FILTER_ENV};
 use hwcodec::{
+    common::{Quality::*, RateControl::*},
     ffmpeg::AVPixelFormat,
     ffmpeg_ram::{
         decode::Decoder,
         encode::{EncodeContext, Encoder},
         CodecInfo,
-        Quality::*,
-        RateControl::*,
     },
+};
+
+#[cfg(feature = "vram")]
+use hwcodec::{
+    common::MAX_GOP,
+    vram::{
+        decode, encode, DecodeContext, DynamicContext, EncodeContext, FeatureContext,
+    }
 };
 use std::time::Instant;
 
+#[cfg(not(feature = "vram"))]
 fn main() {
     init_from_env(Env::default().filter_or(DEFAULT_FILTER_ENV, "info"));
 
@@ -22,11 +30,12 @@ fn main() {
         pixfmt: AVPixelFormat::AV_PIX_FMT_NV12,
         align: 0,
         kbs: 1000,
-        timebase: [1, 30],
+        fps: 30,
         gop: 60,
         quality: Quality_Default,
-        rc: RC_DEFAULT,
+        rc: RC_CBR,
         thread_count: 4,
+        q: -1,
     };
     let start = Instant::now();
     let encoders = Encoder::available_encoders(ctx.clone(), None);
@@ -43,4 +52,24 @@ fn main() {
         CodecInfo::prioritized(decoders.clone())
     );
     log::info!("soft decoders:{:?}", CodecInfo::soft());
+}
+
+
+#[cfg(feature = "vram")]
+fn main() {
+    init_from_env(Env::default().filter_or(DEFAULT_FILTER_ENV, "trace"));
+
+    println!("encoders:");
+    let encoders = encode::available(DynamicContext {
+        width: 1920,
+        height: 1080,
+        kbitrate: 5000,
+        framerate: 30,
+        gop: MAX_GOP as _,
+        device: None,
+    });
+    encoders.iter().map(|e| println!("{:?}", e)).count();
+    println!("decoders:");
+    let decoders = decode::available();
+    decoders.iter().map(|e| println!("{:?}", e)).count();
 }
